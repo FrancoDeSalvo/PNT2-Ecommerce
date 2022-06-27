46f32a589615c9b76a9c9b83b1cb0e6cd901ce3b
Franco <desalvo.fa@gmail.com>
2022-06-27 00:18:53 -0300

    correcciones en carrito

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- <section class="h-100" style="background-color: #eee;">
+<section class="h-100" style="background-color: #eee;">
   <div class="container h-100 py-5">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-10">
@@ -8,37 +8,30 @@
           <h3 class="fw-normal mb-0 text-black">Carrito de compras</h3>
         </div>
 
-      <div :key="p.id" v-for="p in productosData" class = "col-4 mt-4">
-          <ItemCarrito :itemCarrito="p" />
-       
+      <div :key="c.id" v-for="c in items" class="">
+        <ItemCarrito :producto="c" />
+      </div>
 
         <div class="card mb-4">
           <div class="card-body p-4 d-flex flex-row">
-            <div class="form-outline flex-fill">
-              <input type="text" id="form1" class="form-control form-control-lg" />
-              <label class="form-label" for="form1">Discound code</label>
-            </div>
-            <button type="button" class="btn btn-outline-warning btn-lg ms-3">Apply</button>
+            <h4 class="fw-normal mb-0 text-black">Total $ {{this.total}}</h4>
+            <button type="button" class="btn btn-outline-warning btn-lg ms-3">Comprar</button>
           </div>
         </div>
 
-        <div class="card">
-          <div class="card-body">
-            <button type="button" class="btn btn-warning btn-block btn-lg" @click="mostrarProductos">Proceed to Pay</button>
-          </div>
-        </div>
+       
 
       </div>
     </div>
   </div>
-</section> -->
-<div class="container">
+</section>
+<!-- <div class="container">
     <div class="row mb-4 text-dark" id="cards">
       <div :key="c.id" v-for="c in myVar" class="col-4 mt-4">
         <ItemCarrito :producto="c" />
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -49,6 +42,8 @@ export default {
   data() {
     return {
        myVar: this.globalVar,
+       total: 0,
+       items: Array
     };
   },
   props: {
@@ -58,11 +53,52 @@ export default {
     ItemCarrito, 
   },
   methods: {
-    mostrarProductos(){
+    contarUnidades(){
+    let key = "id"
+    let arr2 = [];
+    
+  this.myVar.forEach((x)=>{
+     if(arr2.some((val)=>{ return val[key] == x[key] })){
+       arr2.forEach((k)=>{
+         if(k[key] === x[key]){ 
+           k["occurrence"]++
+           k.nombre = x.nombre
+           k.img = x.img
+           k.precio = x.precio
+          k.precioTotal = x.precio * k.occurrence
+         }
+      })
+         
+     }else{
+  
+       let a = {}
+       a[key] = x[key]
+       a.nombre = x.nombre
+        a.img = x.img
+       a.precio = x.precio
+      //  a.precioTotal = x.precioTotal
+       a["occurrence"] = 1
+       arr2.push(a);
+     }
+  })
+
+    arr2.forEach(element => {
+      console.log(element)
+    });
+    this.items = arr2
+
+    },
+
+
+    calcularTotal(){
       this.myVar.forEach( p => {
-        console.log(p)
+        this.total += p.precio
       });
     }
-  }
+  },
+  created() { 
+    this.calcularTotal();
+    this.contarUnidades();
+  },
 };
 </script>
--- src/components/ItemCarrito.vue
+++ src/components/ItemCarrito.vue
@@ -1,57 +1,46 @@
 <template>
-  <!-- <div class="row justify-content-center">
-    <div class="card text-center" style="width: 18rem;">
-      <router-link :to="url">
-        <img :src= "producto.img" class="card-img-top" alt="Responsive image" style="width: 70%"/>
-        <h5 class="card-title"> {{producto.nombre}}</h5>
-      </router-link>
-      <div class="card-body">
-        <p class="card-text">Precio: ${{producto.precio}}</p>
-      </div>
-    </div>          
-  </div> -->
-<!-- <div class="card rounded-3 mb-4">
+
+<div class="card rounded-3 mb-4">
           <div class="card-body p-4">
             <div class="row d-flex justify-content-between align-items-center">
               <div class="col-md-2 col-lg-2 col-xl-2">
                 <img
-                  src="producto.img"
+                  :src="producto.img"
                   class="img-fluid rounded-3" >
               </div>
               <div class="col-md-3 col-lg-3 col-xl-3">
                 <p class="lead fw-normal mb-2">{{producto.nombre}}</p>
               </div>
               <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                <button class="btn btn-link px-2"
+                <!-- <button class="btn btn-link px-2"
                   onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
                   <i class="fas fa-minus"></i>
                 </button>
 
-                <input id="form1" min="0" name="quantity" value="2" type="number"
+                <input id="form1" min="0" name="quantity" value="1" type="number"
                   class="form-control form-control-sm" />
 
                 <button class="btn btn-link px-2"
                   onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
                   <i class="fas fa-plus"></i>
-                </button>
+                </button> -->
+                <p class="lead fw-normal mb-2">{{producto.occurrence}}</p>
               </div>
               <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                <h5 class="mb-0">{{producto.nombre}}</h5>
-              </div>
+                <h5 class="mb-0">$ {{producto.precio}}  {{producto.precioTotal}} </h5>
+              </div> 
+              
               <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                 <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
               </div>
             </div>
           </div>
-        </div>-->
+        </div>
 
 
- <!-- <div class="container">
-   <h1> ITEM CARRITO </h1>
-   <p>{{producto.nombre}}</p>
-  </div> -->
+ 
 
- <div class="row justify-content-center">
+ <!-- <div class="row justify-content-center">
     <div class="card text-center" style="width: 18rem;">
 
         <img :src= "producto.img" class="card-img-top" alt="Responsive image" style="width: 70%"/>
@@ -61,7 +50,7 @@
     
       </div>
     </div>          
-  </div>
+  </div> -->
 
 </template> 
 
