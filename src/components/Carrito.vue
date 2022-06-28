3834db80c6512817753fdffcf4a858e9b44ef4ec
Franco <desalvo.fa@gmail.com>
2022-06-28 14:29:35 -0300

    modal finalizar carrito

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -23,53 +23,32 @@
         </div>
 
           <div class="card mb-4 text-center">
-            <!-- <div class="card-body p-4 d-flex flex-row justify-content-center"> -->
-
-              <!--<button type="button" class="btn btn-outline-warning btn-lg ms-3">Comprar</button>
-              <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal1"> Generar compra </button>-->
               
               <h4 class="fw-normal mb-0 text-black">Total $ {{this.total}}</h4>
               
               <div class="d-flex mt-2 mb-2 mx-2">
-                <router-link to="/" class="mx-2">
-                  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal1" @click="vaciarCarrito()"> Generar compra </button>
-                </router-link>
-
+                <div class="save-btn">
+                    <button  type="button" class="btn btn-success" @click=" finalizarCarrito()">Generar compra</button>
+                  </div>
                 <router-link to="/Carrito">
                   <button type="button" class="btn btn-danger" @click="vaciarCarrito()"> Vaciar Carrito </button>
                 </router-link>
               </div>
-
-            <!-- </div> -->
           </div>
-
-          <!-- Modal -->
-          <!-- <div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  ...
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div> -->
-
         </div>
       </div>
     </div>
   </section>
+
+  <div>
+    <SavedModal v-show="showModal" @close-modal="showModal = false" />
+  </div>
+
 </template>
 
 <script>
 import ItemCarrito from "../components/ItemCarrito.vue";
+import SavedModal from '../components/SavedModal.vue'
 import axios from "axios";
 export default {
   name: "CarritoProductos",
@@ -81,6 +60,7 @@ export default {
       productos :[],
       perPage: 5,
       currentPage: 1,
+      showModal: false,
     };
   },
   props: {
@@ -88,6 +68,7 @@ export default {
   },
   components: {
     ItemCarrito, 
+    SavedModal,
   },
   computed:{
     productosData() {
@@ -155,8 +136,14 @@ export default {
       this.productos.forEach( p => {
         this.total += p.precio
       });
-    }
+    },
+
+    finalizarCarrito(){
+      this.showModal = true
+      this.vaciarCarrito()
+    },
   },
+  
   async created() { 
     await this.traerProductos();
     await this.calcularTotal();
---
+++ src/components/SavedModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <transition name="modal-fade">
+    <div class="modal-overlay" @click="$emit('close-modal')">
+      
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Carrito Confirmado</h5>
+      </div>
+      <div class="modal-body">
+        <img class="check" src="../assets/succses.png" alt="" />
+        <p>Gracias por elegirnos</p>
+      </div>
+      <div class="modal-footer">
+        <router-link to="/" class="mx-2">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </router-link>
+      </div>
+    </div>
+  </div>
+</div>
+  
+  </transition>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+.check {
+  width: 150px;
+  justify-content: center;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-to {
+  opacity: 0;
+}
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
